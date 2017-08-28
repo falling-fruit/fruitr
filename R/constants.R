bace5374a91bcb2308c9f69cc16514d490bda279
ezwelty <ethan.welty@gmail.com>
2017-08-28 16:11:14 -0600

    Add Unicode zh-hans and zh-hant dictionaries

--- R/constants.R
+++ R/constants.R
@@ -20,7 +20,7 @@ Categories <- c("forager", "freegan", "honeybee", "grafter")
 #'
 #' @export
 #' @family constants
-Location_import_fields <- c('Ids', 'Types', 'Description', 'Lat', 'Lng', 'Address', 'Season Start', 'Season Stop', 'No Season', 'Access', 'Unverified', 'Yield Rating', 'Quality Rating', 'Author', 'Photo URL')
+Location_import_fields <- c("Ids", "Types", "Description", "Lat", "Lng", "Address", "Season Start", "Season Stop", "No Season", "Access", "Unverified", "Yield Rating", "Quality Rating", "Author", "Photo URL")
 
 #' Earth radius
 #'
@@ -37,41 +37,6 @@ Earth_radius <- 6378137
 #' @export
 #' @family constants
 Earth_circumference <- 2 * pi * Earth_radius
-
-#' Language codes
-#'
-#' Comprehensive table of language names and codes.
-#'
-#' \itemize{
-#'   \item locale - Equivalent regional locale, if one exists.
-#'   \item variant - Equivalent script variant, if one exists.
-#'   \item ISO639.1 - \href{https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes}{ISO 639-1} language code.
-#'   \item ISO639.2T - \href{https://en.wikipedia.org/wiki/ISO_639-2}{ISO 639-2T} language code.
-#'   \item ISO639.2B - \href{https://en.wikipedia.org/wiki/ISO_639-2}{ISO 639-2B} language code.
-#'   \item ISO639.3 - \href{https://en.wikipedia.org/wiki/ISO_639-3}{ISO 639-3} language code.
-#'   \item ISO639.6 - \href{https://en.wikipedia.org/wiki/ISO_639-6}{ISO 639-6} language code.
-#'   \item wikipedia - \href{https://en.wikipedia.org/wiki/List_of_Wikipedias}{Wikipedia} language code.
-#'   \item other - Alternative language code.
-#'   \item autonym - Name(s) for the language in the language.
-#'   \item en - English name(s) for the language.
-#'   \item fr - French name(s) for the language.
-#'   \item de - German name(s) for the language.
-#'   \item ru - Russian name(s) for the language.
-#'   \item it - Italian name(s) for the language.
-#'   \item zh - Chinese name(s) for the language.
-#'   \item ISO639.3_macro - \href{https://en.wikipedia.org/wiki/ISO_639_macrolanguage}{ISO 639-3 macrolanguage}.
-#'   \item scope - Language scope (\code{"individual"}, \code{"macro"}, \code{"collective"}, or \code{"special"}).
-#'   \item type - Language type (\code{"living"}, \code{"ancient"}, \code{"historical"}, \code{"extinct"}, \code{"constructed"}, or \code{"special"}).
-#'   \item family - Family to which the language belongs.
-#'   \item retired - Whether the language has been retired.
-#'   \item wikpipedia_url - URL to the wikipedia article for the language.
-#'   \item notes - Additional notes.
-#' }
-#'
-#' @format A \code{data.table}.
-#' @name Language_codes
-#' @family constants
-NULL
 
 #' Google Custom Search Languages
 #'
--- tools/Language_codes.R
+++ tools/Language_codes.R
@@ -1,2 +1,17 @@
+# --- Language_codes ----
+
 Language_codes <- suppressWarnings(data.table::fread("tools/Language_codes.csv", encoding = "UTF-8", na.strings = ""))
 save(Language_codes, file = "data/Language_codes.rda")
+
+# --- Unicode ----
+
+Unicode <- list()
+files <- list.files("tools", "Unicode_.*\\.csv", full.names = TRUE)
+for (file in files) {
+  locale <- regmatches(file, regexec("_(.*)\\.csv", file))[[1]][2]
+  Unicode[[locale]] <- read.csv(file, encoding = "UTF-8", stringsAsFactors = FALSE)[[1]]
+}
+zh_ambiguous <- intersect(Unicode$zh_hant, Unicode$zh_hans)
+Unicode$zh_hant <- setdiff(Unicode$zh_hant, zh_ambiguous)
+Unicode$zh_hans <- setdiff(Unicode$zh_hans, zh_ambiguous)
+save(Unicode, file = "data/Unicode.rda")
