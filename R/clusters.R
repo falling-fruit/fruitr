fe74dab57d12a39a2190b4199d6a97195ddf2d9f
ezwelty <ethan.welty@gmail.com>
2017-09-04 01:53:18 -0600

    Refactor cluster functions

--- R/clusters.R
+++ R/clusters.R
@@ -8,7 +8,6 @@
 #' @return A character vector of the same length as \code{x}.
 #' @export
 #' @seealso \code{\link[base]{strtoi}} for the reverse conversion.
-#' @family cluster functions
 #' @examples
 #' itostr(123, nchars = 4)
 #' strtoi(itostr(123, base = 2), base = 2)
@@ -29,95 +28,79 @@ itostr <- function(x, base = 10L, nchars = 0L) {
    stop(paste("Base", base, "not supported"))
   }
   x <- as.integer(x)
-  return(sprintf(paste0("%0", nchars, "s"), binary(x)))
+  sprintf(paste0("%0", nchars, "s"), binary(x))
 }
 
-#' Convert Coordinates to Grid Cell Indices
+#' Convert between WGS84 and Web Mercator Coordinates
 #'
-#' Converts geographic WGS84 (\href{http://spatialreference.org/ref/epsg/wgs-84/}{EPSG:4326}) coordinates to Web Mercator (\href{http://spatialreference.org/ref/sr-org/7483/}{EPSG:3857}) quadtree grid cell indices.
+#' Converts between geographic WGS84 (\href{http://spatialreference.org/ref/epsg/wgs-84/}{EPSG:4326}) coordinates in decimal degrees and projected Web Mercator (\href{http://spatialreference.org/ref/sr-org/7483/}{EPSG:3857}) coordinates in meters.
 #'
-#' @param lnglat Rows of coordinates in decimal degrees (longitude, latitude), with longitude: [-180, 180) and latitude: [-85.05113, 85.05113).
-#' @param zoom Zoom level(s) of the quadtree dividing the Earth into a square \eqn{2^{zoom}}{2^zoom} grid.
-#' @return Rows of zero-based, integer grid cell indices (x, y, zoom).
+#' @param lnglat Rows of coordinates in decimal degrees (longitude, latitude), with longitude: [-180, 180) and latitude: [-85.05112, 85.05112].
 #' @export
-#' @family cluster functions
+lnglat_to_xy <- function(lnglat) {
+  if (any(lnglat[, 1] < -180 | lnglat[, 1] >= 180)) {
+    stop("Longitudes must be in the range [-180, 180).")
+  }
+  if (any(lnglat[, 2] < -85.05112 | lnglat[, 2] > 85.05112)) {
+    stop("Latitudes must be in the range [-85.05112, 85.05112].")
+  }
+  cbind(
+    x = (lnglat[, 1] / 360) * Earth_circumference,
+    y = log(tan((lnglat[, 2] + 90) * (pi / 360))) * Earth_radius
+  )
+}
+#' @rdname lnglat_to_xy
+#' @param xy Rows of Web Mercator coordinates in meters (x, y).
+#' @export
 #' @examples
 #' lng <- c(-45, -45, 45, 45)
 #' lat <- c(-45, 45, 45, -45)
 #' lnglat <- cbind(lng, lat)
-#' lnglat_to_gridcells(lnglat, zoom = 1)
-lnglat_to_gridcells <- function(lnglat, zoom = 0L) {
-  if (is.vector(lnglat)) {
-    lnglat = t(lnglat[1:2])
-  }
-  if (any(lnglat[, 1] < -180 | lnglat[, 1] >= 180)) {
-    stop("Longitudes must be in the range [-180, 180).")
-  }
-  if (any(lnglat[, 2] < -85.05113 | lnglat[, 2] > 85.05113)) {
-    stop("Latitudes must be in the range [-85.05113, 85.05113).")
-  }
-  # Convert WGS84 to Web Mercator
-  x <- (lnglat[, 1] / 360) * Earth_circumference
-  y <- log(tan((lnglat[, 2] + 90) * (pi / 360))) * Earth_radius
+#' xy_to_lnglat(lnglat_to_xy(lnglat)) - lnglat
+xy_to_lnglat <- function(xy) {
+  cbind(
+    lng = xy[, 1] * (360 / Earth_circumference),
+    lat = 90 - (atan2(1, exp(xy[, 2] / Earth_radius)) * (360 / pi))
+  )
+}
+
+#' Convert between Web Mercator Coordinates and Grid Cell Indices
+#'
+#' Converts between projected Web Mercator (\href{http://spatialreference.org/ref/sr-org/7483/}{EPSG:3857}) coordinates in meters and quadtree grid cell indices. The conversion back from grid cell indices returns the coordinates of the lower left corner of the cell.
+#'
+#' @param xy Rows of Web Mercator coordinates in meters (x, y).
+#' @param zoom Zoom level(s) of the quadtree dividing the Earth into a square \eqn{2^{zoom}}{2^zoom} grid.
+#' @export
+xy_to_gridcells <- function(xy, zoom = 0L) {
   # Move origin to bottom left corner
-  xy <- cbind(x, y) + (Earth_circumference / 2)
-  # Convert to grid cell indices
+  xy <- xy + (Earth_circumference / 2)
   cell_size <- Earth_circumference / (2 ^ zoom)
   xyi <- floor(xy / cell_size)
-  # HACK: Snap yi to 0 for minimum latitude
-  xyi[lnglat[, 2] == -85.05113, 2] <- 0
-  return(cbind(xyi, zoom))
+  cbind(xyi, zoom)
 }
-
-#' Convert Grid Cell Indices to Coordinates
-#'
-#' Converts Web Mercator (\href{http://spatialreference.org/ref/sr-org/7483/}{EPSG:3857}) quadtree grid cell indices to geographic WGS84 (\href{http://spatialreference.org/ref/epsg/wgs-84/}{EPSG:4326}) coordinates.
-#'
+#' @rdname xy_to_gridcells
 #' @param xyz Rows of zero-based, integer grid cell indices (x, y, zoom).
-#' @return Rows of coordinates in decimal degrees (longitude, latitude) corresponding to the lower-left cell corners.
 #' @export
-#' @seealso \code{\link{lnglat_to_gridcells}} for the reverse conversion.
-#' @family cluster functions
 #' @examples
-#' xi <- c(0, 0, 1, 0)
-#' yi <- c(0, 1, 1, 0)
-#' zoom <- c(0, 1, 1, 2)
-#' xyz <- cbind(xi, yi, zoom)
-#' lnglat <- gridcells_to_lnglat(xyz)
-#' xyz == lnglat_to_gridcells(lnglat, zoom)
-gridcells_to_lnglat <- function(xyz) {
-  if (is.vector(xyz)) {
-    xyz = t(xyz[1:3])
-  }
-  # Convert to meters
-  grid_cell_size <- Earth_circumference / (2 ^ xyz[, 3])
-  xy <- xyz[, 1:2] * grid_cell_size
+#' xy <- cbind(x = 0, y = 0)
+#' xyz <- xy_to_gridcells(xy, zoom = 1)
+#' gridcells_to_xy(xyz)
+#' xyz <- xy_to_gridcells(xy, zoom = 0)
+#' gridcells_to_xy(xyz)
+gridcells_to_xy <- function(xyz) {
+  cell_size <- Earth_circumference / (2 ^ xyz[, 3])
+  xy <- xyz[, 1:2, drop = FALSE] * cell_size
   # Move origin to center
-  xy <- xy - (Earth_circumference / 2)
-  # Web Mercator -> WGS84
-  lng <- xy[, 1] * (360 / Earth_circumference)
-  lat <- 90 - (atan2(1, exp(xy[, 2] / Earth_radius)) * (360 / pi))
-  return(cbind(lng, lat))
+  xy - (Earth_circumference / 2)
 }
 
-#' Convert Grid Cell Indices to Geohashes
+#' Convert between Grid Cell Indices and Geohashes
 #'
-#' Converts Web Mercator (\href{http://spatialreference.org/ref/sr-org/7483/}{EPSG:3857}) quadtree grid cell indices to binary geohashes.
+#' Converts between Web Mercator (\href{http://spatialreference.org/ref/sr-org/7483/}{EPSG:3857}) quadtree grid cell indices and binary geohashes.
 #'
 #' @param xyz Rows of Web Mercator grid cell indices (x, y, zoom).
-#' @return A character vector of geohashes.
 #' @export
-#' @family cluster functions
-#' @examples
-#' xi <- c(0, 0, 1, 0)
-#' yi <- c(0, 1, 1, 0)
-#' zoom <- c(0, 1, 1, 2)
-#' xyz <- cbind(xi, yi, zoom)
-#' gridcells_to_geohashes(xyz)
 gridcells_to_geohashes <- function(xyz) {
-  if (is.vector(xyz)) {
-    xyz = t(xyz[1:3])
-  }
   if (any(xyz[, 1:2] >  2 ^ xyz[, 3] - 1)) {
     stop("x, y indices must be in the range [0, 2^zoom)")
   }
@@ -129,20 +112,17 @@ gridcells_to_geohashes <- function(xyz) {
   geohashes <- apply(xyb, 1, function(x) {
     paste0(c(rbind(x[[1]], x[[2]])), collapse = "")
   })
-  return(geohashes)
+  geohashes
 }
-
-#' Convert Geohashes to Grid Cell Indices
-#'
-#' Converts binary geohashes to Web Mercator (\href{http://spatialreference.org/ref/sr-org/7483/}{EPSG:3857}) quadtree grid cell indices.
-#'
+#' @rdname gridcells_to_geohashes
 #' @param geohashes A character vector of geohashes.
-#' @return Rows of Web Mercator grid cell indices (x, y, zoom).
 #' @export
-#' @family cluster functions
-#' @seealso \code{\link{gridcells_to_geohashes}} for the reverse conversion.
 #' @examples
-#' geohashes <- c("00", "0000", "0001", "0010", "001001")
+#' xi <- c(0, 0, 1, 0)
+#' yi <- c(0, 1, 1, 0)
+#' zoom <- c(0, 1, 1, 2)
+#' xyz <- cbind(xi, yi, zoom)
+#' geohashes <- gridcells_to_geohashes(xyz)
 #' xyz <- geohashes_to_gridcells(geohashes)
 #' geohashes == gridcells_to_geohashes(xyz)
 geohashes_to_gridcells <- function(geohashes) {
@@ -161,14 +141,13 @@ geohashes_to_gridcells <- function(geohashes) {
 #' @param geohashes (character vector) Geohashes to expand.
 #' @return Character vector of each geohash in \code{geohashes} followed by its parents.
 #' @export
-#' @family cluster functions
 #' @examples
 #' expand_geohashes("0011")
 #' expand_geohashes(c("0011", "110011"))
-#' xyz <- lnglat_to_gridcells(c(123.4567, 12.3456), 31)
+#' xyz <- xy_to_gridcells(lnglat_to_xy(cbind(123.4567, 12.3456)), 20)
 #' geohashes <- gridcells_to_geohashes(xyz)
 #' exyz <- geohashes_to_gridcells(expand_geohashes(geohashes))
-#' lnglat <- gridcells_to_lnglat(exyz)
+#' lnglat <- xy_to_lnglat(gridcells_to_xy(exyz))
 #' cbind(exyz, lnglat)
 expand_geohashes <- function(geohashes) {
   rows <- 1:length(geohashes)
@@ -179,5 +158,13 @@ expand_geohashes <- function(geohashes) {
     rows <<- c(rows, ind) # WARNING: writing to variable outside apply() scope.
     substr(geohashes[ind], 1, subset_length)
   })))
-  return(expanded[order(rows)])
+  expanded[order(rows)]
+}
+
+#' Calculate Zoom from Geohashes
+#'
+#' @param geohashes Geohashes.
+#' @export
+geohashes_to_zoom <- function(geohashes) {
+  as.integer((nchar(geohashes) / 2) - 1)
 }
